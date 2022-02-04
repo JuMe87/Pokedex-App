c774017680e1f093634975384be966480e4b71bb
JuMe87 <juliane.meyen87@googlemail.com>
2022-02-04 10:26:06 +0100

    Update scripts.js
    Deleted former content and added Pokemon list and four Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-alert('Hello world');
-let favoriteFood = 'Banana';
-document.write(favoriteFood);
+pokemonList = [{
+    name: 'Pikachu',
+    height: 0.4,
+    weight: 6.0,
+    type: ['electric']
+},
+  
+{
+    name: 'Ditto',
+    height: 0.3,
+    weight: 4.0,
+    type: ['normal']
+},
+  
+{
+    name: 'Squirtle',
+    height: 0.5,
+    weight: 9.0,
+    type: ['water']
+} 
+
+{
+    name: 'Jigglypuff',
+    height: 0.5,
+    weight: 5.5,
+    type: ['fairy', 'normal']
+} 
+]
