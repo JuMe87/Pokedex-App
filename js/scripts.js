f9e102a1caa6a4a0c5c537c9b8a79fc3d68fa356
JuMe87 <juliane.meyen87@googlemail.com>
2022-02-11 11:46:58 +0100

    Update scripts.js
    Replaced for() loop with for Each() loop

--- js/scripts.js
+++ js/scripts.js
@@ -31,25 +31,25 @@ let pokemonList = [{
 
 console.log(pokemonList);
 
-// Loop with a conditional that checks whether height is above a certain value
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.6) {
-      console.log(
-          pokemonList[i].name + " " + pokemonList[i].height + " Wow, that's big!"
-        );
-      document.write(
-            "<p>" + 
-            pokemonList[i].name + 
-            " " +
-            pokemonList[i].height + 
-            " Wow, that's big!" + 
-            "</p>"
-        );
-    } else {
-        console.log(pokemonList[i].name + " " + pokemonList[i].height);
-        document.write(
-            "<p>" + pokemonList[i].name + " " + pokemonList[i].height + "</p>"
-        );
-    }
+// Replacing forLoop with a forEach() loop
+function myLoopFunction(pokemon) {
+    console.log(pokemon.name + " " + pokemon.height + " " + pokemon.weight + " "+ pokemon.type);
+    document.write(
+        "<p>" + 
+        pokemon.name + 
+        " " +
+        pokemon.height + 
+        " " +
+        pokemon.weight +
+        " " +
+        pokemon.type + 
+        "</p>"
+    )
 }
+pokemonList.forEach(myLoopFunction);
+
+// Different way of displaying all properties of my Pokemon - I don't like it
+Object.keys(pokemonList).forEach(function(property) {
+    console.log(pokemonList[property]);
+});
 
