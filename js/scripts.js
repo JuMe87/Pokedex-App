01cfe5d8bca870873d8b12a501072c05d7e6fe3c
JuMe87 <juliane.meyen87@googlemail.com>
2022-02-11 12:49:40 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,54 @@
-// List of Pokemon, thier name, height, weight and type, some pokemon have more than one type
-
-let pokemonList = [{
-    name: 'Pikachu',
-    height: 0.4,
-    weight: 6.0,
-    type: ['electric']
-},
+// List of Pokemon wrapped in an IIFE
+let pokemonRepository = (function () {
+    
+    let pokemonList = [
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            weight: 6.0,
+            type: ['electric']
+        },
   
-{
-    name: 'Charizard',
-    height: 1.7,
-    weight: 90.5,
-    type: ['fire', 'flying']
-},
+        {
+            name: 'Charizard',
+            height: 1.7,
+            weight: 90.5,
+            type: ['fire', 'flying']
+        },
   
-{
-    name: 'Squirtle',
-    height: 0.5,
-    weight: 9.0,
-    type: ['water']
-}, 
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            weight: 9.0,
+            type: ['water']
+        }, 
 
-{
-    name: 'Jigglypuff',
-    height: 0.5,
-    weight: 5.5,
-    type: ['fairy', 'normal']
-} 
-];
+        {
+            name: 'Jigglypuff',
+            height: 0.5,
+            weight: 5.5,
+            type: ['fairy', 'normal']
+        } 
+    ]
 
-console.log(pokemonList);
+    function getAll () {
+        return pokemonList;
+    }
+    
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+    
+    return {
+        getAll: getAll,
+        add: add
+    }
 
+})()
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu' });
+ 
 // Replacing for() loop with a forEach() loop
 function myLoopFunction(pokemon) {
     console.log(pokemon.name + " " + pokemon.height + " " + pokemon.weight + " "+ pokemon.type);
@@ -48,8 +66,5 @@ function myLoopFunction(pokemon) {
 }
 pokemonList.forEach(myLoopFunction);
 
-// Different way of displaying all properties of my Pokemon - I don't like it
-Object.keys(pokemonList).forEach(function(property) {
-    console.log(pokemonList[property]);
-});
+
 
